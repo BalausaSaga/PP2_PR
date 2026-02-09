974adb92cbc4d05260fc35885cbba51a27c91b8a
Balausa <bsagatkali@bk.ru>
2026-02-10 00:34:39 +0600

    Переменные класса

---
+++ PP2_pr/practice3/classes/class_variables.py
@@ -0,0 +1,29 @@
+# 1
+class Counter:
+    count = 0
+
+print(Counter.count)
+
+# 2
+class Person:
+    species = "Human"
+
+print(Person.species)
+
+# 3
+class Math:
+    pi = 3.14
+
+print(Math.pi)
+
+# 4
+class Config:
+    debug = True
+
+print(Config.debug)
+
+# 5
+class Student:
+    school = "Oxford"
+
+print(Student.school)
