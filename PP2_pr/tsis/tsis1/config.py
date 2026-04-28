00a2cd747b42e993afb5ba4a7583ad9d93ff79f4
Balausa <bsagatkali@bk.ru>
2026-04-29 00:45:08 +0600

    Add tsis1

---
+++ PP2_pr/tsis/tsis1/config.py
@@ -0,0 +1,8 @@
+# настройки подключения к базе данных
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "database": "phonebook_db",
+    "user": "postgres",
+    "password": "Qwert.yuiop"
+}
